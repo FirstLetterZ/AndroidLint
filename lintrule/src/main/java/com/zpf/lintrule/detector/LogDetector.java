48aaf29d3e0b24fb959e3de59a68082961427d42
Zpf <13967587751@163.com>
2021-03-04 13:55:27 +0800

    插件更新

--- lintrule/src/main/java/com/zpf/lintrule/detector/LogDetector.java
+++ lintrule/src/main/java/com/zpf/lintrule/detector/LogDetector.java
@@ -1,6 +1,5 @@
 package com.zpf.lintrule.detector;
 
-import com.android.tools.lint.client.api.UElementHandler;
 import com.android.tools.lint.detector.api.Category;
 import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Implementation;
@@ -11,69 +10,33 @@ import com.android.tools.lint.detector.api.Severity;
 import com.intellij.psi.PsiMethod;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.uast.UCallExpression;
-import org.jetbrains.uast.UElement;
-import org.jetbrains.uast.util.UastExpressionUtils;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("UnstableApiUsage")
 public class LogDetector extends Detector implements Detector.UastScanner{
 
     public static final Issue ISSUE = Issue.create(
-            "LogUsage",
-            "避免使用android.util.Log",
-            "请使用统一的日志工具类!",
-            Category.CORRECTNESS,
-            6,
-            Severity.ERROR,
-            new Implementation(LogDetector.class, Scope.JAVA_FILE_SCOPE)
+            "LogUsage",//id:唯一值，在配置lint屏蔽规则时会使用到
+            "Avoid to use android.util.Log",//简短的描述
+            "请使用统一的日志工具类!",//对问题的解释，需要注意汉字编码问题可能导致乱码
+            Category.CORRECTNESS,//问题类别
+            6,//优先级：1-10，数字越大级别越高（越严重）
+            Severity.ERROR,//严重程度：Fatal, Error, Warning, Informational, Ignore
+            new Implementation(LogDetector.class, Scope.JAVA_FILE_SCOPE)//执行规制：包含探测器和探测范围2个参数
     );
 
-    @Nullable
     @Override
-    public List<Class<? extends UElement>> getApplicableUastTypes() {
-        return Collections.singletonList(UCallExpression.class);
+    public List<String> getApplicableMethodNames() {
+        return Arrays.asList("v", "d", "i", "w", "e", "wtf");
     }
 
-    @Nullable
     @Override
-    public UElementHandler createUastHandler(@NotNull JavaContext context) {
-        return new LogHandler(context);
-    }
-
-    static class LogHandler extends UElementHandler {
-
-        private final JavaContext context;
-
-        LogHandler(JavaContext context) {
-            this.context = context;
-        }
-
-        @Override
-        public void visitCallExpression(@NotNull UCallExpression node) {
-            if (!UastExpressionUtils.isMethodCall(node)) return;
-            if (node.getReceiver() != null
-                    && node.getMethodName() != null) {
-                String methodName = node.getMethodName();
-                if (methodName.equals("i")
-                        || methodName.equals("d")
-                        || methodName.equals("e")
-                        || methodName.equals("v")
-                        || methodName.equals("w")
-                        || methodName.equals("wtf")) {
-                    PsiMethod method = node.resolve();
-                    if (context.getEvaluator().isMemberInClass(method, "android.util.Log")) {
-                        context.report(ISSUE,
-                                node,
-                                context.getLocation(node),
-                                "\u21E2 避免使用android.util.Log"
-                        );
-                    }
-                }
-            }
+    public void visitMethodCall(@NotNull JavaContext context, @NotNull UCallExpression node, @NotNull PsiMethod method) {
+        if (context.getEvaluator().isMemberInClass(method, "android.util.Log")) {
+            context.report(ISSUE, node, context.getLocation(node), "避免调用android.util.Log");
         }
     }
 }
